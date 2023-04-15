b567c8053b329fcdc55ebffb763fef6fb0208875
littlepig2013 <zhuzichen19960417@gmail.com>
2023-04-15 16:23:34 -0400

    update readme

--- exp/exp.sh
+++ exp/exp.sh
@@ -1,58 +1,23 @@
 #!/bin/bash
 
-TRIES=1
+#Intro Exp
+python3 vary-buffer-size-emul-fig1.py --OP="emul_vary_buffer_size_128-250000-mu-1-tau-1-nodirectio.txt"  --tries 3
+python3 vary-buffer-size-emul-fig1.py --OP="emul_vary_buffer_size_zipf_128-250000-mu-1-tau-1-nodirectio.txt" --JD 3 --tries 3
 
-echo "Baisc exp.."
-python3 vary-buffer-size.py --tries ${TRIES}
-echo "Baisc exp done"
+#Exp 1
+python3 vary-buffer-size-emul.py --OP="emul_vary_buffer_size_256-262144-mu-2.9-tau-2.1-nosyncio-run2.txt"  --tries 3
+python3 vary-buffer-size-emul.py --OP="emul_vary_buffer_size_zipf_alpha_1.3_256-262144-mu-2.9-tau-2.1-nosyncio-run2.txt" --JD 3 --JD_ZALPHA 1.3 --tries 3
+python3 vary-buffer-size-emul.py --OP="emul_vary_buffer_size_zipf_alpha_1.0_256-262144-mu-2.9-tau-2.1-nosyncio-run2.txt" --JD 3 --JD_ZALPHA 1.0 --tries 3
+python3 vary-buffer-size-emul.py --OP="emul_vary_buffer_size_zipf_alpha_0.7_256-262144-mu-2.9-tau-2.1-nosyncio-run2.txt" --JD 3 --JD_ZALPHA 0.7 --tries 3
 
-echo "Varying lTS.."
-python3 vary-buffer-size.py --lTS 4000000 --OP vary_buffer_size-lTS-4M.txt --tries ${TRIES}
-python3 vary-buffer-size.py --lTS 2000000 --OP vary_buffer_size-lTS-2M.txt --tries ${TRIES}
-python3 vary-buffer-size.py --lTS 500000 --OP vary_buffer_size-lTS-0.5M.txt --tries ${TRIES}
-echo "Varying lTS done"
+#Exp 2
+python3 vary-buffer-size-emul-fig7-directio.py --OP="emul_vary_buffer_size_256-262144-mu-5-tau-3.5.txt"  --tries 3
+python3 vary-buffer-size-emul-fig7-directio.py --OP="emul_vary_buffer_size_zipf_alpha_1.3_256-262144-mu-5-tau-3.5.txt" --JD 3 --JD_ZALPHA 1.3 --tries 3
+python3 vary-buffer-size-emul-fig7-directio.py --OP="emul_vary_buffer_size_zipf_alpha_1.0_256-262144-mu-5-tau-3.5.txt" --JD 3 --JD_ZALPHA 1.0 --tries 3
+python3 vary-buffer-size-emul-fig7-directio.py --OP="emul_vary_buffer_size_zipf_alpha_0.7_256-262144-mu-5-tau-3.5.txt" --JD 3 --JD_ZALPHA 0.7 --tries 3
 
 
 
-#<<COMMENT
-echo "Varying rTS.."
-python3 vary-buffer-size.py --rTS 4000000 --OP vary_buffer_size-rTS-4M.txt --tries ${TRIES}
-python3 vary-buffer-size.py --rTS 16000000 --OP vary_buffer_size-rTS-16M.txt --tries ${TRIES}
-python3 vary-buffer-size.py --rTS 32000000 --OP vary_buffer_size-rTS-32M.txt --tries ${TRIES}
-echo "Varying rTS done"
-#COMMENT
-
-#<<COMMENT
-
-echo "Varying key size.."
-python3 vary-buffer-size.py -K 256 --OP vary_buffer_size-K-256B.txt --tries ${TRIES}
-python3 vary-buffer-size.py -K 512 --OP vary_buffer_size-K-512B.txt --tries ${TRIES}
-echo "Varying key size done"
-
-#COMMENT
-
-echo "Varying lE.."
-python3 vary-buffer-size.py --lE 128 --OP vary_buffer_size-lE-128B.txt --tries ${TRIES}
-python3 vary-buffer-size.py --lE 256 --OP vary_buffer_size-lE-256B.txt --tries ${TRIES}
-python3 vary-buffer-size.py --lE 512 --OP vary_buffer_size-lE-512B.txt --tries ${TRIES}
-python3 vary-buffer-size.py --lE 2048 --OP vary_buffer_size-lE-2KB.txt --tries ${TRIES}
-echo "Varying lE done"
-echo "Varying rE.."
-python3 vary-buffer-size.py --rE 128 --OP vary_buffer_size-rE-128B.txt --tries ${TRIES}
-python3 vary-buffer-size.py --rE 256 --OP vary_buffer_size-rE-256B.txt --tries ${TRIES}
-python3 vary-buffer-size.py --rE 512 --OP vary_buffer_size-rE-512B.txt --tries ${TRIES}
-python3 vary-buffer-size.py --rE 2048 --OP vary_buffer_size-rE-2KB.txt --tries ${TRIES}
-echo "Varying rE done"
-echo "Varying distribution.."
-python3 vary-buffer-size.py --JD 1 --OP vary_buffer_size-normal.txt --tries ${TRIES}
-python3 vary-buffer-size.py --JD 1 --JD_NDEV 0.25 --OP vary_buffer_size-normal-dev-0.25.txt --tries ${TRIES}
-python3 vary-buffer-size.py --JD 1 --JD_NDEV 0.5 --OP vary_buffer_size-normal-dev-0.5.txt --tries ${TRIES}
-python3 vary-buffer-size.py --JD 1 --JD_NDEV 2 --OP vary_buffer_size-normal-dev-2.txt --tries ${TRIES}
-#python3 vary-buffer-size.py --JD 2 --OP vary_buffer_size-beta.txt --tries ${TRIES}
-python3 vary-buffer-size.py --JD 3 --JD_ZALPHA 0 --OP vary_buffer_size-zipf-alpha-0.txt --tries ${TRIES}
-python3 vary-buffer-size.py --JD 3 --JD_ZALPHA 0.5 --OP vary_buffer_size-zipf-alpha-0.5.txt --tries ${TRIES}
-python3 vary-buffer-size.py --JD 3 --JD_ZALPHA 1.5 --OP vary_buffer_size-zipf-alpha-1.5.txt --tries ${TRIES}
-python3 vary-buffer-size.py --JD 3 --OP vary_buffer_size-zipf.txt --tries ${TRIES}
-python3 vary-buffer-size.py --JD 3 --JD_ZALPHA 2 --OP vary_buffer_size-zipf-alpha-2.txt --tries ${TRIES}
-echo "Varying distribution done"
-
+#Exp 3
+python3 vary-buffer-size-emul-fig8.py --OP="emul_vary_buffer_size_128-512-mu-2.9-tau-2.1-nosyncio.txt"  --tries 3
+python3 vary-buffer-size-emul-fig8.py --OP="emul_vary_buffer_size_zipf_alpha_1.0_128-512-mu-2.9-tau-2.1-nosyncio.txt"  --tries 3 --JD 3 --JD_ZALPHA 1.0
--- exp/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 vary-buffer-size-emul.py --lTS=4000 --rTS=32000 --OP vary_buffer_size-288-352-BNLJ-first-4000_rTS-32000.txt --tries 10
-python3 vary-buffer-size-emul.py --lTS=5000 --rTS=40000 --OP vary_buffer_size-288-352-BNLJ-first-5000_rTS-40000.txt --tries 10
-python3 vary-buffer-size-emul.py --lTS=6000 --rTS=48000 --OP vary_buffer_size-288-352-BNLJ-first-6000_rTS-48000.txt --tries 10
